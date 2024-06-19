1fa36797bcec4d86745c1d96130ce2a0962c2e63
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-19 15:25:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def double_char(s):
         result += char * 2
     return result
 
-print(double_char("String"))
+# print(double_char("String"))
 
 # 8 kyu Sum Mixed Array
 # Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
@@ -17,7 +17,7 @@ def sum_mix(arr):
         total_sum += int(num)
     return total_sum
 
-print(sum_mix([9, 3, '7', '3']))
+# print(sum_mix([9, 3, '7', '3']))
 
 # 8 kyu Third Angle of a Triangle
 # You are given two interior angles (in degrees) of a triangle.
@@ -28,11 +28,36 @@ def other_angle(a, b):
     # third_angle = total_angle - (a+b)
     return 180 - (a+b)
 
-print(other_angle(30, 60))
+# print(other_angle(30, 60))
 
 # 8 kyu Exclamation marks series #1: Remove an exclamation mark from the end of string
 # Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
 def remove(s):
     return s.removesuffix('!') 
 
-print(remove("Hi!"))
+# print(remove("Hi!"))
+
+# 8 kyu Is it a palindrome?
+# Write a function that checks if a given string (case insensitive) is a palindrome.
+# A palindrome is a word, number, phrase, or other sequence of symbols that reads the 
+# same backwards as forwards, such as madam or racecar.
+def is_palindrome(s):
+    s = s.lower()
+    reversed_s = s[::-1]
+    return s == reversed_s
+
+# print(is_palindrome('aba'))
+
+# 8 kyu Difference of Volumes of Cuboids
+# In this simple exercise, you will create a program that will take two lists of integers, a and b. 
+# Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. 
+# You must find the difference of the cuboids' volumes regardless of which is bigger.
+# For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 
+# and the volume of b is 20. Therefore, the function should return 8.
+# Your function will be tested with pre-made examples as well as random ones.
+def find_difference(a, b):
+    volume_a = a[0]*a[1]*a[2]
+    volume_b = b[0]*b[1]*b[2]
+    return abs(volume_a - volume_b)
+
+print(find_difference([3, 2, 5], [1, 4, 4]))
