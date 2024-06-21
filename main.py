caf7de1d29cb23bd1c453eb2e18811f120cc5cdd
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-21 15:31:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def double_char(s):
         result += char * 2
     return result
 
+
 # print(double_char("String"))
 
 # 8 kyu Sum Mixed Array
@@ -17,6 +18,7 @@ def sum_mix(arr):
         total_sum += int(num)
     return total_sum
 
+
 # print(sum_mix([9, 3, '7', '3']))
 
 # 8 kyu Third Angle of a Triangle
@@ -26,19 +28,22 @@ def sum_mix(arr):
 def other_angle(a, b):
     # total_angle = 180
     # third_angle = total_angle - (a+b)
-    return 180 - (a+b)
+    return 180 - (a + b)
+
 
 # print(other_angle(30, 60))
 
-# 8 kyu Exclamation marks series #1: Remove an exclamation mark from the end of string
-# Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+# 8 kyu Exclamation marks series #1: Remove an exclamation mark from the end of string Remove an exclamation mark
+# from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to
+# verify it.
 def remove(s):
-    return s.removesuffix('!') 
+    return s.removesuffix('!')
+
 
 # print(remove("Hi!"))
 
 # 8 kyu Is it a palindrome?
-# Write a function that checks if a given string (case insensitive) is a palindrome.
+# Write a function that checks if a given string (case-insensitive) is a palindrome.
 # A palindrome is a word, number, phrase, or other sequence of symbols that reads the 
 # same backwards as forwards, such as madam or racecar.
 def is_palindrome(s):
@@ -46,19 +51,21 @@ def is_palindrome(s):
     reversed_s = s[::-1]
     return s == reversed_s
 
+
 # print(is_palindrome('aba'))
 
 # 8 kyu Difference of Volumes of Cuboids
 # In this simple exercise, you will create a program that will take two lists of integers, a and b. 
 # Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. 
 # You must find the difference of the cuboids' volumes regardless of which is bigger.
-# For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 
+# For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of an is 12
 # and the volume of b is 20. Therefore, the function should return 8.
 # Your function will be tested with pre-made examples as well as random ones.
 def find_difference(a, b):
-    volume_a = a[0]*a[1]*a[2]
-    volume_b = b[0]*b[1]*b[2]
-    return abs(volume_a - volume_b) 
+    volume_a = a[0] * a[1] * a[2]
+    volume_b = b[0] * b[1] * b[2]
+    return abs(volume_a - volume_b)
+
 
 # print(find_difference([3, 2, 5], [1, 4, 4]))
 
@@ -69,6 +76,7 @@ def find_difference(a, b):
 def plural(n):
     return n != 1
 
+
 # print(plural(1))
 
 # 8 kyu altERnaTIng cAsE <=> ALTerNAtiNG CaSe
@@ -77,4 +85,25 @@ def to_alternating_case(string):
     return string.swapcase()
 
 
-print(to_alternating_case("hello WORLD"))
+# print(to_alternating_case("hello WORLD"))
+
+# 8 kyu What is between?
+# Complete the function that takes two integers (a, b, where a < b)
+# and return an array of all integers between the input parameters, including them.
+def between(a, b):
+    return list(range(a, b + 1))
+
+
+# print(between(1, 4))
+
+# 8 kyu Beginner Series #1 School Paperwork
+# Your classmates asked you to copy some paperwork for them.
+# You know that there are 'n' classmates and the paperwork has 'm' pages.
+# Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+def paperwork(n, m):
+    if n < 0 or m < 0:
+        return 0
+    return n * m
+
+
+print(paperwork(-5, 5))
