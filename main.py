b782ec6a27171c18371820dad5f0913e25b37e79
RomaSamkov <Romansamkov.rs@gmail.com>
2024-06-27 13:15:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -186,4 +186,35 @@ def remove_exclamation_marks(s):
     return s.replace('!', '')
 
 
-print(remove_exclamation_marks("Hello World!"))
+# print(remove_exclamation_marks("Hello World!"))
+
+# 8 kyu Student's Final Grade Create a function finalGrade, which calculates the final grade of a student depending
+# on two parameters: a grade for the exam and a number of completed projects. This function should take two
+# arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above); This
+# function should return a number (final grade). There are four types of final grades: 100, if a grade for the exam
+# is more than 90 or if a number of completed projects more than 10. 90, if a grade for the exam is more than 75 and
+# if a number of completed projects is minimum 5. 75, if a grade for the exam is more than 50 and if a number of
+# completed projects is minimum 2. 0, in other cases
+def final_grade(exam, projects):
+    if exam > 90 or projects > 10:
+        return 100
+    elif exam > 75 and projects >= 5:
+        return 90
+    elif exam > 50 and projects >= 2:
+        return 75
+    else:
+        return 0
+
+
+# print(final_grade(100, 12))
+
+# 8 kyu Add Length What if we need the length of the words separated by a space to be added at the end of that same
+# word and have it returned as an array? Your task is to write a function that takes a String and returns an
+# Array/list with the length of each word added to each element .
+def add_length(str_):
+    words = str_.split()
+    result = [f"{word} {len(word)}"for word in words]
+    return result
+
+
+print(add_length('apple ban'))
