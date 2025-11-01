957e2be3445737ed79d79fd9fa84091b895b13fc
RomaSamkov <Romansamkov.rs@gmail.com>
2025-11-01 20:09:28 +0200

    add function mango

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # 8 kyu
-# Grasshopper - Combine strings
 # Combine strings function
 # Create a function named combineNames/combine_names/CombineNames that accepts
 # two parameters (first and last name). The function should return the full name.
@@ -14,3 +13,14 @@ def combine_names(first, last):
 
 
 print(combine_names("James", "Stevens"))
+
+
+# Accountant time! For a given quantity and price (per mango),
+# calculate the total cost of the mangoes.
+# But! Every third mango is free!
+
+def mango(quantity, price):
+    return price * (quantity - (quantity // 3))
+
+
+print(mango(3, 3))
