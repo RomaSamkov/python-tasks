5c7a08b3513878e62af5fa62793a091557671035
RomaSamkov <Romansamkov.rs@gmail.com>
2025-11-04 20:30:11 +0200

    add function remove_url_anchor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sum_two_smallest_numbers(numbers):
-    return sum(sorted(numbers)[:2])
+def remove_url_anchor(url):
+    return url.split('#')[0]
 
 
-print(sum_two_smallest_numbers([19, 5, 42, 2, 77]))
+print(remove_url_anchor("www.codewars.com#about"))
